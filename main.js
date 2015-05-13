1d7451b88af3f0611fee49316641e17e9b809e5c
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-12 23:31:23 -0400

    Begins JavaScript functions

---
+++ main.js
@@ -0,0 +1,13 @@
+var inputOperator = document.querySelector(".operator");
+var addInteger = document.querySelector("button");
+var addOperator = document.querySelector(".operator");
+var evaluate = document.querySelector(".equals");
+var addDecimal = document.querySelector(".decimal");
+var clear = document.querySelector(".clear");
+var screen = document.querySelector("view");
+
+
+
+
+addInteger.addEventListener("click", performCalc)
+addOperator.addEventListener("click", performCalc)
